43beb1777be5bf4e743e9d961eee0153060afd1e
陈天程 <93662748+Aumuyanzzz@users.noreply.github.com>
2021-11-05 23:12:22 +0800

    Add files via upload

---
+++ 11.5练习草稿.cpp
@@ -0,0 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+////} 
+//int main()
+//{
+//	int a = 0;
+//	int ret = 0;
+//	char password[30] = { 0 };
+//	while (a < 3)
+//	{
+//		int ch = 0;
+//		printf("请输入新密码:>");
+//		scanf("%s", password);
+//		
+//		while((ch = getchar()) != '\n');
+//		{
+//
+//		}
+//		printf("是否确认设置为新密码[Y/N]:>\n");
+//		ret = getchar();
+//		if (ret == 'Y') 
+//		{
+//			int t = 0;
+//			printf("修改成功，新密码：\n"),
+//				printf("%s", password);
+//			while (t = getchar()!=  '\n');
+//			{
+//             }
+//			break;
+//		}
+//		else if (ret == 'N')
+//		{
+//			printf("放弃确认\n");
+//			while (getchar() != '\n');
+//			{
+//
+//			}
+//
+//			continue;
+//		}
+//		
+//		else
+//		{
+//			printf("错误输入\n");
+//			while (getchar() != '\n');
+//			{
+//
+//			}
+//			continue;
+//		}
+//	}
+////		
+////
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int ret = 0;
+//	char password[10] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//	getchar();
+//	printf("请确认[Y/N]:>");
+//	ret = getchar();
+//	if(ret == 'Y')
+//	{
+//		printf("确认成功\n");
+//
+//	}
+//	else
+//	{
+//
+//		printf("放弃确认\n");
+//	}
+//	return 0;
+//int main()
+//{
+//	int a = 0;
+//	while (ch = getchar() != EOF)
+//
+//	{
+//		if (ch < '0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}//输出0到9字符
+//int main()
+//{
+//	int a = 0;
+//	for (a = 1; a <= 10 || a >= 1; a++)
+//	{
+//
+//		printf("%d\n", a);
+//	}
+//	return 0;
+//}  //for(表达式①；表达式②；表达式③）{表达式②假执行代码块、表达式③}
+int main()
+{
+	for (;;)
+	{
+
+		printf("hehe\n");
+	}
+	return 0;
+}//for循环的初始化、调整、判断都可以省略
+//for的循环部分如果被省略，那判断恒 真
